08c3d6a8d08fb34eb520721c3de3fa5c6212849a
ryo-yoshizawa <r2yoshizawa@lab.miraikan.jst.go.jp>
2023-06-30 14:35:44 +0900

    Initial Commit

---
+++ Cabot-web-app/LocalWebView.swift
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2023
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *******************************************************************************/
+
+import UIKit
+import WebKit
+import SwiftUI
+
+struct LocalWebView: UIViewRepresentable{
+
+    @Binding var reload: Bool
+
+    private let webview = WKWebView()
+
+    fileprivate func loadRequest(in webView: WKWebView) {
+        
+        let theFileName = ("cabot_map" as NSString).lastPathComponent
+        let htmlPath = Bundle.main.path(forResource: theFileName, ofType: "html")
+        let folderPath = Bundle.main.bundlePath
+        let baseUrl = URL(fileURLWithPath: folderPath, isDirectory: true)
+
+        do {
+            let htmlString = try NSString(contentsOfFile: htmlPath!, encoding: String.Encoding.utf8.rawValue)
+            webView.loadHTMLString(htmlString as String, baseURL: baseUrl)
+        } catch {
+        }
+        webView.isOpaque = false
+        webView.isHidden = false
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<LocalWebView>) -> WKWebView {
+        loadRequest(in: webview)
+        return webview
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<LocalWebView>) {
+        if reload {
+            loadRequest(in: uiView)
+            DispatchQueue.main.async {
+                self.reload = false
+            }
+        }
+    }
+}
